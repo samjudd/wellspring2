33fd43f5a9e59e0610f01f43e9551f01f5f4d844
Samuel Judd <samjudd@protonmail.com>
2021-03-07 16:04:29 -0800

    another bugfix

--- Skirmisher/Skirmisher.cs
+++ Skirmisher/Skirmisher.cs
@@ -75,4 +75,10 @@ public class Skirmisher : Player
       _rightGunAmmo = _gunClipSize;
     }
   }
+
+  // need this here so that godot can see it to have it as a callback
+  protected override void JumpCallback()
+  {
+    _vel.y = _jumpSpeed;
+  }
 }
